3e0b82c080636ed735b29bcf4e9d6f2bfba0bf7f
Jianwen WEI <weijianwen@gmail.com>
2014-06-02 15:17:36 +0800

    Regenerate all PDF documents with context workflow. Resize the fontsize in typing environment -- bodyfont=9.0pt. Make all links in middleblue color. Update vpn.mkd

--- tex/context.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-DOC=ssh_context
-
-pandoc -f markdown -t context --toc  --smart --standalone --template=../pandoc/zh.context ../mkd/ssh.mkd -o ${DOC}.tex
-rm ${DOC}.pdf
-context --purge
-context ${DOC} && open ${DOC}.pdf
-
